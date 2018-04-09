de480f07cb20f86ecbc18d505b490e5d798db86b
Nathan Fleurisma <nfleurisma@gmail.com>
2018-04-09 16:45:17 -0400

    new updates

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const morgan = require('morgan');
 const artistRoutes = require('./routes/artists');
+const songRoutes = require('./routes/songs');
 
 app.use(express.json()); // Body parser!
 app.use(morgan('combined'));
@@ -12,6 +13,7 @@ app.get('/', (request, response) => {
 });
 
 app.use('/artists', artistRoutes);
+app.use('/songs', songRoutes);
 
 app.listen( PORT, () => {
   console.log(`Artists and Songs: listening on port no. ${PORT}...`);
