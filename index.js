4c4d048b0b45495ca38f05180ebbcad82d90ec7d
Nathan Fleurisma <nfleurisma@gmail.com>
2018-04-09 16:05:43 -0400

    CRUD

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const morgan = require('morgan');
-const artistRoutes = require('./routes/artists';)
+const artistRoutes = require('./routes/artists');
+
+app.use(express.json()); // Body parser!
 app.use(morgan('combined'));
 
-app.get('/',(request, response) => {
-  response.send('Welcome to the record store.');
+app.get('/', (request, response) => {
+  response.send('Welcome to my Record Store.');
 });
-app.use('/artists', artistsRoutes);
-app.listen(PORT, () => {
+
+app.use('/artists', artistRoutes);
+
+app.listen( PORT, () => {
   console.log(`Artists and Songs: listening on port no. ${PORT}...`);
 });
